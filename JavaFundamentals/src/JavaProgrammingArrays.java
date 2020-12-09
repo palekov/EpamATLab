165b8baf1b9f8e679a5524967e08141ec5c6e01d
Alexander <palekov-2011@mail.ru>
2020-12-09 18:22:28 +0300

    added some comments

--- JavaFundamentals/src/JavaProgrammingArrays.java
+++ JavaFundamentals/src/JavaProgrammingArrays.java
@@ -16,20 +16,20 @@ public class JavaProgrammingArrays {
         String str = Arrays.toString(array);
         int hash = Arrays.hashCode(array);
 
-        System.out.println(str);
-        System.out.println(hash);
+        System.out.println(str);        // проверяем как работает  Arrays.toString
+        System.out.println(hash);       // выводим hashCode
 
-        for(int i = 0; i < stringArray.length; i++)
+        for(int i = 0; i < stringArray.length; i++)    //  используем Array.length
             System.out.println(stringArray[i]);
         for(int i = 0; i < 5; i++) {
             array[i] =i;
-            System.out.println(array[i]);
+            System.out.println(array[i]);             // выводим элементы по индексу
         }
 
         System.out.println();
-        System.out.println(array1.equals(array2));
-        System.out.println(Arrays.equals(array1, array2));
+        System.out.println(array1.equals(array2));           // сравниваем как ссылки
+        System.out.println(Arrays.equals(array1, array2));   // сравниваем как массивы
 
-        System.out.println(array[15]);
+        System.out.println(array[15]);   // получаем ArrayIndexOutOfBoundsException
     }
 }
--- JavaFundamentals/src/JavaProgrammingObjectClass.java
+++ JavaFundamentals/src/JavaProgrammingObjectClass.java
@@ -5,7 +5,7 @@ public class JavaProgrammingObjectClass {
         int varA;
         int varB;
 
-        BlackBox(int varA, int varB){
+        BlackBox( int varA, int varB ) {
             this.varA = varA;
             this.varB = varB;
         }
@@ -51,24 +51,24 @@ public class JavaProgrammingObjectClass {
         BlackBox object2 = new BlackBox(5, 10);
         BlackBox object3 = new BlackBox(7, 15);
         BlackBox object4 = null;
+        // работаем с equals и hashCode
+        System.out.println(object1.equals(object2));                    //  true
+        System.out.println(object1.hashCode() == object2.hashCode());   //  true
+        System.out.println(object1.equals(object3));                    // false
+        System.out.println(object1.hashCode() == object3.hashCode());   // false
 
-        System.out.println(object1.equals(object2));
-        System.out.println(object1.hashCode() == object2.hashCode());
-        System.out.println(object1.equals(object3));
-        System.out.println(object1.hashCode() == object3.hashCode());
-
-        System.out.println(object1.getClass());
+        System.out.println(object1.getClass());      // используем getClass()
         System.out.println(object2.getClass());
         System.out.println(object3.getClass());
 
-        System.out.println(object1);
+        System.out.println(object1);                 // используем toString
         System.out.println(object2);
         System.out.println(object3);
 
-        classObj = object1.getClass();
+        classObj = object1.getClass();               // используем Class class
         System.out.println(classObj.getName());
         System.out.println(classObj.getPackage());
 
-        System.out.println(object4.toString());
+        System.out.println(object4.toString());     // получаем NullPointerException
     }
 }
