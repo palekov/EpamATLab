8c8d93b22300cda4d94b4794cfb311b0cce447bb
Alexander <palekov-2011@mail.ru>
2020-12-22 20:29:24 +0300

    java strings added

--- JavaFundamentals/src/JavaStrings.java
+++ JavaFundamentals/src/JavaStrings.java
@@ -1,3 +1,4 @@
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 
 public class JavaStrings {
@@ -25,8 +26,8 @@ public class JavaStrings {
         char chars[] = { 'a', 'b', 'c' };
         String str_cha = new String(chars);               //  creates string from char[]
 
-        byte ascii[] = { 65, 66, 67, 68, 69, 70 };
-        String str_byt = new String(ascii);               //  creates string 'ABCDEF' from byte[]
+        byte utf16[] = { 0, 1, 2, 3, 4, 5, 6, 7 };
+        String str_byt = new String(utf16, StandardCharsets.UTF_16);   // creates string from byte[]
         System.out.println(str_byt);
 
         String str = str_lit + str_obj;                   //  concatenate, using '+'
@@ -92,5 +93,35 @@ public class JavaStrings {
             default:
                 System.out.println("not a number");
         }
+
+        String ss1 = "Eleven";
+        String ss2 = new String("Eleven");
+        String ss3 = new String("Eleven");
+        String ss4 = "Eleven";
+        System.out.println(ss1 == ss2);             // false, because links to two different objects
+        System.out.println(ss1 == ss4);             // true, because its one object from string pool
+        System.out.println(ss1.equals(ss4));        // true, because the contents of the objects are identical
+        System.out.println(ss1.intern() == ss4.intern());   // true, because ss1.equals(ss4) is true
+                                                            // but this method is faster with frequent comparison
+        System.out.println(ss1.equals(ss2));        // true, because equals compares the contents
+        ss2 = ss1;
+        System.out.println(ss1 == ss2);             // true, because its links to one object
+        System.out.println(ss2 != ss3);             // true, because its two different objects
+        System.out.println(ss2.equals(ss3));        // true, because the contents of the objects are identical
+        System.out.println(ss2.intern() == ss3.intern());   //  true, because ss2.equals(ss3) is true
+
+        StringBuffer sb = new StringBuffer("this is a string buff ");
+        sb.append(new Integer(1));
+        sb.append("; ");
+        sb.append(true);
+        sb.append(" it is synchronized!");
+        System.out.println(sb);                     //  outputs 'this is a string buff 1; true it is synchronized!'
+
+        StringBuilder sb2 = new StringBuilder("this is s string build ");
+        sb2.append(new Integer(101112));
+        sb2.append("; ");
+        sb2.append(false);
+        sb2.append(" it is not synchronized!");      //  outputs 'this is s string build 101112; false it is not synchronized!'
+        System.out.println(sb2);
     }
 }
