174b4642e6633dd163d219c9c8f2508795233ed8
Alexander <palekov-2011@mail.ru>
2021-01-31 22:07:08 +0300

    java nested&lambdas added

--- JavaFundamentals/src/JavaNestedLambdas.java
+++ JavaFundamentals/src/JavaNestedLambdas.java
@@ -1,15 +1,34 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.BiFunction;
 
 public class JavaNestedLambdas {
 
-    //  write lambdas that implement the functional interfaces: Consumer, Supplier, Function, Predicate
-    //  write lambdas with and without parameters
-    //  write lambdas with and without body
-    //  write lambdas that return values implicitly and explicitly
+    int num;
+    String type;
+    static int staticTest = 10;
+    int instanceTest = 20;
+
+    public JavaNestedLambdas(int num, String type) {
+        this.num = num;
+        this.type = type;
+    }
+
+    public JavaNestedLambdas() {
+    }
+
     //  write functional interface, mark it with the @FunctionalInterface annotation and create lambda of its type
-    //  show that lambda has access to static and instance fields/methods of the enclosing context
-    //  show that lambda has access to final local variables
-    //  show that lambda has access to effectively final local variables
+    @FunctionalInterface
+    interface MyInterface {
+        double getPiValue();
+    }
+
+    @FunctionalInterface
+    interface MyFunc {
+        int func(int n);
+    }
 
     @FunctionalInterface
     public interface Predicate<T> {
@@ -31,9 +50,50 @@ public class JavaNestedLambdas {
         R apply(T t);
     }
 
+    private static void staticMethod(String s) {
+        // do something
+    }
+
+    private void instanceMethod(String s) {
+        // do something
+    }
 
     public static void main(String[] args) {
 
+        List<String> strings = new LinkedList<>();
+        strings.add("мама");
+        strings.add("мыла");
+        strings.add("раму");
+
+        //  write lambdas with and without parameters
+        strings.forEach(x -> System.out.println(x));
+
+        MyInterface ref = () -> 3.1415;
+
+        Runnable runnable = () -> System.out.println("Я - лямбда!");
+        runnable.run();
+
+        //  show that lambda has access to static and instance fields/methods of the enclosing context
+        Supplier<Integer> supplier = () -> staticTest++;
+        JavaNestedLambdas instance1 = new JavaNestedLambdas();
+        Supplier<Integer> supplier1 = () -> instance1.instanceTest++;
+
+        //  show that lambda has access to final local variables
+        final int j = 7;
+        MyFunc myLambda1 = (n) -> {
+            int z = j + 1;
+            return z;
+        };
+
+        //  show that lambda has access to effectively final local variables
+        int num = 10;
+        MyFunc myLanbda2 = (n) -> {
+            int v = num + n;
+            //  num++;          error:  Variable used in lambda expression should be final or effectively final
+            return v;
+        };
+
+        //  write lambdas that implement the functional interfaces: Consumer, Supplier, Function, Predicate
         //  using Predicate
         Predicate<Integer> isEvenNumber = x -> x % 2==0;
         System.out.println(isEvenNumber.test(4));       //  outputs 'true'
@@ -60,5 +120,20 @@ public class JavaNestedLambdas {
         String s = "678";
         Function valueConverter = x -> Integer.valueOf((String) x);
         System.out.println(valueConverter.apply(s));    //  outputs '678'
+
+        //  write method reference to a static method
+        strings.forEach(JavaNestedLambdas::staticMethod);
+
+        //  write method reference to an instance method of a particular object
+        JavaNestedLambdas instance = new JavaNestedLambdas();
+        strings.forEach(instance::instanceMethod);
+
+        //  write method reference to an instance method of an arbitrary object of a particular type
+        List<Integer> numbers = Arrays.asList(5, 3, 50, 24, 40, 2, 9, 18);
+        numbers.stream().sorted(Integer::compareTo);
+
+        //  write method reference to a constructor
+        BiFunction<Integer, String, JavaNestedLambdas> aNew = JavaNestedLambdas::new;
+
     }
 }
