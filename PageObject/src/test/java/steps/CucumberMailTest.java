5e60ff6d12364d2de94684bd591fcc60dab8a3e1
Aliaksandr Paliakou1 <aliaksandr_paliakou1@epam.com>
2021-05-12 09:43:54 +0300

    cucumber beta

--- PageObject/src/test/java/steps/CucumberMailTest.java
+++ PageObject/src/test/java/steps/CucumberMailTest.java
@@ -1,11 +1,11 @@
-package cucumber;
+package steps;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(strict = true, plugin = {"json:target/cucumber-report.json"},
-              features = "src/test/resources/cucumber/yahoo-mail-test.feature")
-             //   glue = "src/test/java/steps")
+              features = "src/test/resources/cucumber/yahoo-mail-test.feature",
+                glue = "src/test/java/steps")
 
 public class CucumberMailTest extends AbstractTestNGCucumberTests {
 
